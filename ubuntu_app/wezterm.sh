845836f1894643fe603a1036ec4490cb13849fa4
trungnq2710 <trungnq2710@gmail.com>
2023-02-07 11:24:10 +0700

    feat(wezterm): update

--- ubuntu_app/wezterm.sh
+++ ubuntu_app/wezterm.sh
@@ -1,5 +1,9 @@
-curl -LO https://github.com/wez/wezterm/releases/download/20221119-145034-49b9839f/wezterm-20221119-145034-49b9839f.Ubuntu22.04.deb
-sudo apt install -y ./wezterm-20221119-145034-49b9839f.Ubuntu22.04.deb
+PACKAGE_NAME=wezterm-20221119-145034-49b9839f.Ubuntu22.04.deb
+
+curl -LO https://github.com/wez/wezterm/releases/download/20221119-145034-49b9839f/$PACKAGE_NAME
+sudo apt install -y ./$PACKAGE_NAME
 
 # change default term ubuntu
 sudo update-alternatives --config x-terminal-emulator
+
+rm -rf ./$PACKAGE_NAME
