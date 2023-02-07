7244b275f947ac0a1c7d40377cf297312ccf3ff4
trungnq2710 <trungnq2710@gmail.com>
2023-02-07 12:05:28 +0700

    feat: update

--- ibus-bamboo/install.sh
+++ ibus-bamboo/install.sh
@@ -1,6 +1,6 @@
 sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
 sudo apt-get update
-sudo apt-get install ibus ibus-bamboo --install-recommends
+sudo apt-get install -y ibus ibus-bamboo --install-recommends
 ibus restart
 
 env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['BambooUs', 'Bamboo']" && gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
--- ubuntu_app/wezterm.sh
+++ ubuntu_app/wezterm.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+
 PACKAGE_NAME=wezterm-20221119-145034-49b9839f.Ubuntu22.04.deb
 
+echo "+ Downloading $PACKAGE_NAME ..."
 curl -LO https://github.com/wez/wezterm/releases/download/20221119-145034-49b9839f/$PACKAGE_NAME
+
+echo "+ Installing ..."
 sudo apt install -y ./$PACKAGE_NAME
 
-# change default term ubuntu
+echo "+ Change default term ubuntu"
 sudo update-alternatives --config x-terminal-emulator
 
+echo "+ Cleaning.."
 rm -rf ./$PACKAGE_NAME
+
+echo "+ Install success"
